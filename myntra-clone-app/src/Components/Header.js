7324758d3ff8d63a512aaf0c356a400a703aaba1
akshayswami17 <akshayswami1731@gmail.com>
2024-11-06 17:40:35 +0530

    .

--- myntra-clone-app/src/Components/Header.js
+++ myntra-clone-app/src/Components/Header.js
@@ -1,38 +1,58 @@
+// import { BsFillPersonFill } from "react-icons/bs";
+// import { FaFaceGrinHearts, FaBagShopping } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Header = () => {
-    return (
-        <div>
-            <div className="logo_container">
-                <a href="#"><img className="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home" /></a>
-            </div>
-            <nav className="nav_bar">
-                <a href="#">Men</a>
-                <a href="#">Women</a>
-                <a href="#">Kids</a>
-                <a href="#">Home & Living</a>
-                <a href="#">Beauty</a>
-                <a href="#">Studio <sup>New</sup></a>
-            </nav>
-            <div className="search_bar">
-                <span className="material-symbols-outlined search_icon">search</span>
-                <input className="search_input" placeholder="Search for products, brands and more" />
-            </div>
-            <div className="action_bar">
-                <div className="action_container">
-                    <span className="material-symbols-outlined action_icon">person</span>
-                    <span className="action_name">Profile</span>
-                </div>
-                <div className="action_container">
-                    <span className="material-symbols-outlined action_icon">favorite</span>
-                    <span className="action_name">Wishlist</span>
-                </div>
-                <a className="action_container" href="pages/bag.html">
-                    <span className="material-symbols-outlined action_icon">shopping_bag</span>
-                    <span className="action_name">Bag</span>
-                    {/* <span className="bag-item-count">{bagItemCount}</span> */}
-                </a>
-            </div>
+  const bag = useSelector((store) => store.bag);
+
+  return (
+    <header>
+      <div className="logo_container">
+        <Link to="/">
+          <img
+            className="myntra_home"
+            src="images/myntra_logo.webp"
+            alt="Myntra Home"
+          />
+        </Link>
+      </div>
+      <nav className="nav_bar">
+        <a href="#">Men</a>
+        <a href="#">Women</a>
+        <a href="#">Kids</a>
+        <a href="#">Home & Living</a>
+        <a href="#">Beauty</a>
+        <a href="#">
+          Studio <sup>New</sup>
+        </a>
+      </nav>
+      <div className="search_bar">
+        <span className="material-symbols-outlined search_icon">search</span>
+        <input
+          className="search_input"
+          placeholder="Search for products, brands and more"
+        />
+      </div>
+      <div className="action_bar">
+        <div className="action_container">
+          {/* <BsFillPersonFill /> */}
+          <span className="action_name">Profile</span>
         </div>
-    );
-}
+
+        <div className="action_container">
+          {/* <FaFaceGrinHearts /> */}
+          <span className="action_name">Wishlist</span>
+        </div>
+
+        <Link className="action_container" to="/bag">
+          {/* <FaBagShopping /> */}
+          <span className="action_name">Bag</span>
+          <span className="bag-item-count">{bag.length}</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
